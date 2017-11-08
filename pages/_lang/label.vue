03a7817916b03f0c5203c5e931eae092512367f6
christopheschwyzer <kingchiller@gmail.com>
2017-11-09 00:12:25 +0100

    Restructure label page

--- pages/_lang/label.vue
+++ pages/_lang/label.vue
@@ -1,14 +1,13 @@
 <template>
 <div id="label">
-  <section class="intro">
+  <section class="intro mission">
     <div class="section-inner boundaries" v-scroll-reveal="{
     scale: 1,
     delay: 200,
     distance: '20px'
   }">
       <h1>{{ $t('pages.label') }}</h1>
-      <div class="abstract" v-html="$t('intro.abstract')"></div>
-      <img class="logo" src="~assets/images/logo.svg" alt="Logo">
+      <div class="abstract" v-html="$t('mission.abstract')"></div>
     </div>
     <div class="more">
       <v-icon>expand_more</v-icon>
@@ -23,8 +22,30 @@
     delay: 200,
     distance: '20px'
   }">
-        <h2>{{ $t('mission.heading')}}</h2>
-        <div class="abstract" v-html="$t('mission.abstract')"></div>
+        <div class="benefits">
+          <h2>{{ $t('benefits.heading')}}</h2>
+          <div class="abstract">
+            {{ $t('benefits.abstract')}}
+          </div>
+          <v-layout wrap class="benefits-list">
+            <v-flex md6 class="box">
+              <div class="box-inner">
+                <h4>{{ $t('benefits.consumers.heading') }}</h4>
+                <ul>
+                  <li v-for="(item, i) in $t('benefits.consumers.arguments')" :key="i">{{ item }}</li>
+                </ul>
+              </div>
+            </v-flex>
+            <v-flex md6 class="box">
+              <div class="box-inner">
+                <h4>{{ $t('benefits.manufacturers.heading') }}</h4>
+                <ul>
+                  <li v-for="(item, i) in $t('benefits.manufacturers.arguments')" :key="i">{{ item }}</li>
+                </ul>
+              </div>
+            </v-flex>
+          </v-layout>
+        </div>
       </div>
     </div>
     <div class="wave wave-bottom"></div>
@@ -36,30 +57,16 @@ scale: 1,
 delay: 200,
 distance: '20px'
 }">
-      <div class="benefits">
-        <h2>{{ $t('benefits.heading')}}</h2>
-        <div class="abstract">
-          {{ $t('benefits.abstract')}}
-        </div>
-        <v-layout wrap>
-          <v-flex md6 class="box">
-            <div class="box-inner">
-              <h4>{{ $t('benefits.consumers.heading') }}</h4>
-              <ul>
-                <li v-for="(item, i) in $t('benefits.consumers.arguments')" :key="i">{{ item }}</li>
-              </ul>
-            </div>
-          </v-flex>
-          <v-flex md6 class="box">
-            <div class="box-inner">
-              <h4>{{ $t('benefits.manufacturers.heading') }}</h4>
-              <ul>
-                <li v-for="(item, i) in $t('benefits.manufacturers.arguments')" :key="i">{{ item }}</li>
-              </ul>
-            </div>
-          </v-flex>
-        </v-layout>
-      </div>
+      <h2>{{ $t('values.heading')}}</h2>
+      <div class="abstract" v-html="$t('values.abstract', {mib: '<strong>Made in Basel</strong>'})"></div>
+      <v-layout wrap>
+        <v-flex md4 class="box" v-for="(item, i) in $t('values.values')" :key="i">
+          <div class="box-inner">
+            <h4>{{ item.heading }}</h4>
+            <p>{{ item.description }}</p>
+          </div>
+        </v-flex>
+      </v-layout>
     </div>
   </section>
 
@@ -67,42 +74,25 @@ distance: '20px'
     <div class="wave wave-top"></div>
     <div class="section-inner">
       <div class="boundaries">
-        <h2>{{ $t('values.heading')}}</h2>
-        <div class="abstract" v-html="$t('values.abstract', {mib: '<strong>Made in Basel</strong>'})"></div>
-        <v-layout wrap>
-          <v-flex md4 class="box" v-for="(item, i) in $t('values.values')" :key="i">
-            <div class="box-inner">
-              <h4>{{ item.heading }}</h4>
-              <p>{{ item.description }}</p>
-            </div>
-          </v-flex>
-        </v-layout>
+        <h2>{{ $t('vision.heading')}}</h2>
+        <div class="abstract" v-html="$t('vision.abstract')"></div>
       </div>
     </div>
     <div class="wave wave-bottom"></div>
   </section>
 
-  <section>
-
+  <section class="brand">
     <div class="section-inner">
       <div class="boundaries" v-scroll-reveal="{
       scale: 1,
       delay: 200,
       distance: '20px'
     }">
-        <h2>{{ $t('vision.heading')}}</h2>
-        <div class="abstract" v-html="$t('vision.abstract')"></div>
+        <h2>{{ $t('brand.heading')}}</h2>
+        <div class="abstract" v-html="$t('brand.abstract')"></div>
+        <img class="logo" src="~assets/images/logo.svg" alt="Logo">
       </div>
 
-    </div>
-  </section>
-
-
-  <section class="section--small section--dark">
-    <div class="section-inner">
-      <div class="boundaries">
-        <small>{{ $t('intro.small') }}</small>
-      </div>
     </div>
   </section>
 </div>
@@ -119,12 +109,7 @@ export default {
   i18n: {
     messages: {
       en: {
-        intro: {
-          abstract: 'The main aspect of <strong>Made in Basel</strong> is the trade-mark shown below. It serves as a label and a distinctive brand. The use of the label is free of charge for all institutions and residents of Basel*.',
-          small: '*See terms for more details.'
-        },
         mission: {
-          heading: 'Mission',
           abstract: '<strong>Made in Basel</strong> is an initiative to promote products and services from Basel. We support innovation and entrepreneurial spirit, sustain a diverse, creative climate where ideas and people can prosper. The label stands for high-quality local products and services with a unique spirit. Members are committed to Basel as a place to live, work and recreate.'
         },
         benefits: {
@@ -167,18 +152,17 @@ export default {
             }
           ]
         },
+        brand: {
+          heading: 'Brand',
+          abstract: 'The main aspect of <strong>Made in Basel</strong> is the trade-mark shown below. It serves as a label and a distinctive brand. The use of the label is free of charge for all institutions and residents of Basel.'
+        },
         vision: {
           heading: 'Vision',
           abstract: '"Whoever has visions should go to the doctor." - <em>Helmut Schmidt</em>'
         }
       },
       de: {
-        intro: {
-          abstract: 'Hauptbestandteil von <strong>Made in Basel</strong> ist die unten abgebildete geschützte Wort-Bild-Marke. Sie dient als Label und als Erkennungsmerkmal. Die Verwendung des Labels steht grundsätzlich allen in Basel ansässigen Institutionen und Personen frei*.',
-          small: '*Unter Berücksichtigung der in den AGBs beschriebenen Voraussetzungen.'
-        },
         mission: {
-          heading: 'Mission',
           abstract: '<strong>Made in Basel</strong> ist eine Initiative um Produkte und Dienstleistungen aus Basel zu bewerben. Wir fördern Innovation und Unternehmergeist, setzen uns für ein vielfältiges, kreatives Klima ein und unterstützen Basels Qualitäten als Wohn-, Arbeits- und Kulturstadt. Das Label steht für hochwertige lokale Produkte und Dienstleistungen mit einem Hauch Basel. Mitglieder bekennen sich zum Werkplatz Basel. '
         },
         benefits: {
@@ -220,6 +204,10 @@ export default {
               description: 'Wir denken global, handeln lokal, glauben an Liberalismus, offene Grenzen und die Idee, in “Vielfalt vereint" zu sein.'
             }
           ]
+        },
+        brand: {
+          heading: 'Marke',
+          abstract: 'Hauptbestandteil von <strong>Made in Basel</strong> ist die unten abgebildete geschützte Wort-Bild-Marke. Sie dient als Label und als Erkennungsmerkmal. Die Verwendung des Labels steht grundsätzlich allen in Basel ansässigen Institutionen und Personen frei.'
         },
         vision: {
           heading: 'Vision',
