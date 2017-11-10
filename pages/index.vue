c3d537409b2de20ccbb5c20095e090ed49e5f9cf
christopheschwyzer <kingchiller@gmail.com>
2017-11-11 00:50:11 +0100

    Move initial language detection to index page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template></template>
+<script>
+import _ from 'underscore'
+export default {
+  beforeMount() {
+    var lang = navigator.language || navigator.userLanguage
+    var self = this
+    if (this.$route.fullPath === '/') {
+      var locale = lang.substring(0, 2) === 'de' ? 'de' : 'en'
+      _.once(function () {
+        self.$router.replace({ path: locale })
+      }())
+    }
+  }
+}
+</script>
